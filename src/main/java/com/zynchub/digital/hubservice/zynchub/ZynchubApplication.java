4544248c779177db37a2811220aa4782fb7af75b
kavalanaresh <kavalanaresh@gmail.com>
2026-01-07 21:41:43 +0000

    Draft hub service repo

---
+++ src/main/java/com/zynchub/digital/hubservice/zynchub/ZynchubApplication.java
@@ -0,0 +1,13 @@
+package com.zynchub.digital.hubservice.zynchub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZynchubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZynchubApplication.class, args);
+	}
+
+}
