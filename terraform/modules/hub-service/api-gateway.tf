0f6acff7741eb2b01f1ad8a40e1767b26b4a7357
kavalanaresh <kavalanaresh@gmail.com>
2026-01-11 21:11:27 +0000

    fix
    fix

--- terraform/modules/hub-service/api-gateway.tf
+++ terraform/modules/hub-service/api-gateway.tf
@@ -26,6 +26,20 @@ resource "aws_api_gateway_deployment" "hub_deploy" {
 
 resource "aws_api_gateway_stage" "hub_stage" {
   deployment_id = aws_api_gateway_deployment.hub_deploy.id
-  rest_api_id   = aws_api_gateway_rest_api.hub_api.id
+  rest_api_id   = aws_api_gateway_rest_api.hub_api.id # Now using .id correctly
   stage_name    = var.environment
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+    format          = jsonencode({
+      requestId      = "$context.requestId"
+      ip             = "$context.identity.sourceIp"
+      requestTime    = "$context.requestTime"
+      httpMethod     = "$context.httpMethod"
+      resourcePath   = "$context.resourcePath"
+      status         = "$context.status"
+      protocol       = "$context.protocol"
+      responseLength = "$context.responseLength"
+    })
+  }
 }
