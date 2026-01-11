65ede05bc16fc154009ed77b3edb8bc07e6cb146
kavalanaresh <kavalanaresh@gmail.com>
2026-01-11 20:53:40 +0000

    Update backend.tf

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "zynchub-hub-terraform-state"
     key            = "hub-service/dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-1"
     dynamodb_table = "zynchub-terraform-locks"
     encrypt        = true
   }
